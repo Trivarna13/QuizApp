6910749997b0a70e9890d0a7ff4c72dc09653f0f
Trivarna13 <104765948+Trivarna13@users.noreply.github.com>
2023-07-05 19:54:13 +0530

    JavaScript

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+let questions = [
+	{
+		numb: 1,
+		question: "Which is the biggest continent in the world?",
+		answer: "Asia",
+		options: ["North America", "Asia", "Africa", "Australia"],
+	},
+	{
+		numb: 2,
+		question: "Which is longest river in the world?",
+		answer: "Nile",
+		options: ["Niger", "Great Ganga", "Amazon", "Nile"],
+	},
+	{
+		numb: 3,
+		question: "Which is the largest ocean in the world?",
+		answer: "Pacific Ocean",
+		options: [
+			"Pacific Ocean",
+			"Arctic Ocean",
+			"Indian Ocean",
+			"Atlantic Ocean",
+		],
+	},
+	{
+		numb: 4,
+		question: "Which is India`s first super computer?",
+		answer: "Param8000",
+		options: ["param80000", "param800", "param8", "Param8000"],
+	},
+	{
+		numb: 5,
+		question: "Which bank is called bankers bank of India?",
+		answer: "Reserve Bank of India",
+		options: [
+			"Reserve Bank of India",
+			"Punjab National Bank",
+			"ICICI Bank",
+			"State Bank of India",
+		],
+	},
+	{
+		numb: 6,
+		question: "Which is largest animal in the world?",
+		answer: "Blue whale",
+		options: ["Giraffe", "Blue whale", "Elephant", "Shark"],
+	},
+	{
+		numb: 7,
+		question: "Which is largest island in the world?",
+		answer: "Greenland",
+		options: ["Hawaii", "Andaman Nicobar", "Greenland", "New Guinea"],
+	},
+	{
+		numb: 8,
+		question: "Which is the 29th state of India?",
+		answer: "Telangana",
+		options: [
+			"Uttar Pradesh",
+			"Uttarakhand",
+			"Telangana",
+			"Madhya Pradesh",
+		],
+	},
+	{
+		numb: 9,
+		question: "Which continent is known as Dark Continent?",
+		answer: "Africa",
+		options: ["Africa", "Asia", "Australia", "South America"],
+	},
+	{
+		numb: 10,
+		question: "Which is the highest dam of India?",
+		answer: "Tehri Dam",
+		options: [
+			"Sardar Sarovar Dam",
+			"Rihand Dam",
+			"Tehri Dam",
+			"Mettur Dam",
+		],
+	},
+];
